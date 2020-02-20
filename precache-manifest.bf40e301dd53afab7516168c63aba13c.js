8d5fa3e97bac0490e4a10a045d9105a8c4af5f41
Thomas Chaplin <thomas.chaplin@tray.io>
2020-02-20 22:59:40 +0000

    Updates

---
+++ precache-manifest.bf40e301dd53afab7516168c63aba13c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829ecb862e6c03e07ab3000f8c34c9cd",
+    "url": "/currency-convertor/index.html"
+  },
+  {
+    "revision": "b8acf848822751b02cb7",
+    "url": "/currency-convertor/static/css/main.512e2be0.chunk.css"
+  },
+  {
+    "revision": "1c6c35a0573186f346f3",
+    "url": "/currency-convertor/static/js/2.3b3ead95.chunk.js"
+  },
+  {
+    "revision": "bc1606c6db11c396205f31e2fe0b9d0c",
+    "url": "/currency-convertor/static/js/2.3b3ead95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8acf848822751b02cb7",
+    "url": "/currency-convertor/static/js/main.c9ea4dd4.chunk.js"
+  },
+  {
+    "revision": "0864e528b98f28e9f384",
+    "url": "/currency-convertor/static/js/runtime-main.c487f96f.js"
+  }
+]);
